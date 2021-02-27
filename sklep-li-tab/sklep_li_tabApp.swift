c055818443298ca3efeaf0225fa3aaab2395e338
Leszek Iwanowski <iwanowskileszek@gmail.com>
2021-02-27 18:20:55 +0100

    Initial Commit

---
+++ sklep-li-tab/sklep_li_tabApp.swift
@@ -0,0 +1,17 @@
+//
+//  sklep_li_tabApp.swift
+//  sklep-li-tab
+//
+//  Created by Leszek Iwanowski on 27/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct sklep_li_tabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
