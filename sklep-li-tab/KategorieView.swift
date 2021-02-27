86ebca779fb3a97a48b991ea36f84682c1e31b98
Leszek Iwanowski <iwanowskileszek@gmail.com>
2021-02-27 18:29:07 +0100

    Początek sklepu tabView

---
+++ sklep-li-tab/KategorieView.swift
@@ -0,0 +1,20 @@
+//
+//  KategorieView.swift
+//  sklep-li-tab
+//
+//  Created by Leszek Iwanowski on 27/02/2021.
+//
+
+import SwiftUI
+
+struct KategorieView: View {
+    var body: some View {
+        Text("Kategorie")
+    }
+}
+
+struct KategorieView_Previews: PreviewProvider {
+    static var previews: some View {
+        KategorieView()
+    }
+}
---
+++ sklep-li-tab/KoszykView.swift
@@ -0,0 +1,20 @@
+//
+//  KoszykView.swift
+//  sklep-li-tab
+//
+//  Created by Leszek Iwanowski on 27/02/2021.
+//
+
+import SwiftUI
+
+struct KoszykView: View {
+    var body: some View {
+        Text("Koszyk")
+    }
+}
+
+struct KoszykView_Previews: PreviewProvider {
+    static var previews: some View {
+        KoszykView()
+    }
+}
