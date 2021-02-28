379b4c983a2ccadbd8bcfd146b68bbf11758b636
Leszek Iwanowski <iwanowskileszek@gmail.com>
2021-02-28 01:50:32 +0100

    Prosty Koszyk

--- sklep-li-tab/KoszykView.swift
+++ sklep-li-tab/KoszykView.swift
@@ -9,35 +9,41 @@ import SwiftUI
 
 struct KoszykView: View {
     
-    @State var koszyk = [
-        "Kalafior"
+//    @State var koszyk = [
+//        "Kalafior"
+//    ]
+    @State var koszyk: [Produkt] = [
+        Produkt(name: "Kalafior", iloscWKoszyku: 5)
     ]
-     
+
+    
+    
     
     var body: some View {
         VStack {
-            List {
-                Section(header: Text("Towary w koszyku:")) {
-                    ForEach(koszyk, id: \.self) { string in
-                        CustomRow(string: string, produkt: Produkt(name: string))
-                    }
-                    .onDelete { (IndexSet) in
-                        delete(at: IndexSet)
-                        
-                    }
-                }
+            List(koszyk){ produkt in
+                KoszykRow(produkt: produkt)
+                    
             }
         }
     }
-    
-    func delete(at indexSet: IndexSet) {
-        
-    }
-    
 }
+
 
 struct KoszykView_Previews: PreviewProvider {
     static var previews: some View {
         KoszykView()
+    }
+}
+
+struct KoszykRow: View {
+    var produkt: Produkt
+
+    var body: some View {
+        HStack {
+            Text(produkt.name)
+            Text(String(produkt.iloscWKoszyku))
+            
+        }
     }
 }
